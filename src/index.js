c9fff683430d44ccb7648e3550fad98340e4d63b
Siarhei <solimr111@mail.ru>
2018-01-21 14:33:17 +0300

    deleted old solution

--- src/index.js
+++ src/index.js
@@ -16,83 +16,9 @@ module.exports = function multiply(first, second) {
             if (!answer[i + 1]) {
                 answer[i + 1] = 0;
             }
-            answer[i + 1] += parseInt(answer[i] / 10);
+            answer[i + 1] += Math.floor(answer[i] / 10);
             answer[i] %= 10;
         }
     }
     return answer.reverse().join('');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  let bigNumber = [],lessNumber = [], remainder = 0 , answer = "";
-  if(first.length > second.length){
-    bigNumber = first.split("");
-    lessNumber = second.split("");
-  }
-  else {
-    bigNumber = second.split("");
-    lessNumber = first.split("");
-  }
-  for (let i = lessNumber.length-1; i > -1; i--) {
-    for (let j = bigNumber.length-1; j > -1; j--) {
-      if (lessNumber[i] === 0 && bigNumber[j] === 0) {
-        answer += '' + '0';
-        break;
-      }
-      if (lessNumber[i] * bigNumber[j] > 9) {
-        remainder = Math.floor((lessNumber[i] * bigNumber[j]) / 10);
-        answer += (lessNumber[i] * bigNumber[j]) % 10;        
-      }
-        else {
-          if (lessNumber[i] * bigNumber[j] + remainder > 9) {
-            answer += Math.floor(lessNumber[i] + bigNumber[j] + remainder); 
-            remainder = (lessNumber[i] + bigNumber[j] + remainder) % 10;
-          }
-          else{
-        answer += '' + lessNumber[i] * bigNumber[j];
-        }
-        }
-      if (j === 0 && remainder !== 0) {
-        answer += '' +remainder;
-      }
-    }
-  }
-  return answer.split('').reverse().join('');   */
-
